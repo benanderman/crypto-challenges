f1351ae527166f91b30be498872004b7637941eb
Ben Anderman <bena@evidentpoint.com>
2016-02-10 15:49:47 -0500

    Initial Commit

---
+++ CryptoChellenges/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CryptoChellenges
+//
+//  Created by Ben Anderman on 2/10/16.
+//  Copyright © 2016 Ben Anderman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
