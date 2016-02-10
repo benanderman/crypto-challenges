32ce9bfc7564c034db7b071f5c87e6119ef87888
Ben Anderman <bena@evidentpoint.com>
2016-02-10 16:05:49 -0500

    Add implementations of the first 4 challenges

---
+++ CryptoChellenges/crypto.swift
@@ -0,0 +1,148 @@
+//
+//  crypto.swift
+//  CryptoChellenges
+//
+//  Created by Ben Anderman on 2/10/16.
+//  Copyright © 2016 Ben Anderman. All rights reserved.
+//
+
+import Foundation
+
+enum AsciiRange: Int {
+  case Lower, Upper, Space, Bad, Other
+  init(value: UInt8) {
+    self = Other
+    if value >= "a".utf8.first! && value <= "z".utf8.first! {
+      self = Lower
+    }
+    if value >= "A".utf8.first! && value <= "Z".utf8.first! {
+      self = Upper
+    }
+    if value == " ".utf8.first! {
+      self = Space
+    }
+    if (value < 10) {
+      self = Bad
+    }
+  }
+}
+
+class Crypto {
+  func hexToBase64(hex: [UInt8]) -> [UInt8]? {
+    if let rawValues = hexToRaw(hex) {
+      var result: [UInt8] = [UInt8]()
+      for var i = 0; i < rawValues.count; i += 3 {
+        let char1 = rawValues[i + 0] >> 2
+        let char2 = ((rawValues[i + 0] << 6) >> 2) | rawValues[i + 1] >> 4
+        let char3 = ((rawValues[i + 1] << 4) >> 2) | rawValues[i + 2] >> 6
+        let char4 = rawValues[i + 2] & (~0 >> 2)
+        result.appendContentsOf([char1, char2, char3, char4].map({ (char) -> UInt8 in
+          if char < 26 {
+            return "A".utf8.first! + char
+          }
+          if char < 26 * 2 {
+            return "a".utf8.first! + (char - 26)
+          }
+          if (char < 26 * 2 + 10) {
+            return "0".utf8.first! + (char - 26 * 2)
+          }
+          return char == 62 ? "+".utf8.first! : "/".utf8.first!
+        }))
+      }
+      
+      return result
+    }
+    return nil
+  }
+  
+  func xorHexStrings(hex1: [UInt8], hex2: [UInt8]) -> [UInt8]? {
+    if (hex1.count != hex2.count) {
+      return nil
+    }
+    if let raw1 = hexToRaw(hex1), let raw2 = hexToRaw(hex2) {
+      var result: [UInt8] = [UInt8]()
+      for (var i = 0; i < raw1.count; i++) {
+        result.append(raw1[i] ^ raw2[i])
+      }
+      return result
+    }
+    return nil
+  }
+  
+  func decipherSingleByteXorHex(inputs: [[UInt8]]) -> [UInt8]? {
+    var bestScore = -100
+    var bestResult = [UInt8]()
+    for hex in inputs {
+      if let raw = hexToRaw(hex) {
+        for var i = 0; i < 256; i++ {
+          let deciphered = decipherSingleByteXor(raw, key: UInt8(i))
+          let score = textScoreForString(deciphered)
+          if (score > bestScore) {
+            bestScore = score
+            bestResult = deciphered
+          }
+        }
+      }
+    }
+    return bestResult
+  }
+  
+  func textScoreForString(text: [UInt8]) -> Int {
+    var counts: Dictionary<AsciiRange, Int> = Dictionary<AsciiRange, Int>()
+    for char in text {
+      let range = AsciiRange(value: char)
+      if let count = counts[range] {
+        counts[range] = count + 1
+      } else {
+        counts[range] = 1
+      }
+    }
+    
+    var score = 0
+    let lowerCount = (counts[AsciiRange.Lower] != nil) ? counts[AsciiRange.Lower]! : 0
+    let upperCount = (counts[AsciiRange.Upper] != nil) ? counts[AsciiRange.Upper]! : 0
+    let spaceCount = (counts[AsciiRange.Space] != nil) ? counts[AsciiRange.Space]! : 0
+    let badCount = (counts[AsciiRange.Lower] != nil) ? counts[AsciiRange.Lower]! : 0
+    
+    score += Int((1 - abs(Double(lowerCount) / Double(text.count) - 0.8)) * 10)
+    score += Int((1 - abs(Double(upperCount) / Double(text.count) - 0.1)) * 5)
+    score += Int((1 - abs(Double(spaceCount) / Double(text.count) - 0.1)) * 10)
+    score += Int((1 - abs(Double(badCount) / Double(text.count))) * -50)
+    
+    //    if let string = String(bytes: text, encoding: NSUTF8StringEncoding) {
+    //      print(string + ": " + String(score))
+    //    }
+    
+    return score
+  }
+  
+  func decipherSingleByteXor(string: [UInt8], key: UInt8) -> [UInt8] {
+    return string.map { $0 ^ key }
+  }
+  
+  func hexToRaw(hex: [UInt8]) -> [UInt8]? {
+    if (hex.count % 2 != 0) {
+      return nil // Not a valid hex string
+    }
+    var rawValues: [UInt8] = [UInt8]()
+    for var i = 0; i < hex.count; i += 2 {
+      rawValues.append(self.hexCharToInt(hex[i]) * 16 + self.hexCharToInt(hex[i + 1]))
+    }
+    return rawValues
+  }
+  
+  func hexCharToInt(char: UInt8) -> UInt8 {
+    let zero = "0".utf8.first!
+    let nine = "9".utf8.first!
+    let a = "a".utf8.first!
+    let f = "f".utf8.first!
+    
+    if (char >= zero && char <= nine) {
+      return char - zero
+    }
+    if (char >= a && char <= f) {
+      return char - a + 10
+    }
+    return 0
+  }
+}
--- CryptoChellenges/main.swift
+++ CryptoChellenges/main.swift
@@ -8,5 +8,42 @@
 
 import Foundation
 
-print("Hello, World!")
+let crypto: Crypto = Crypto()
 
+let base64TestInput = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+var result = String(bytes: crypto.hexToBase64([UInt8](base64TestInput.utf8))!, encoding: NSUTF8StringEncoding)
+if let output = result {
+  print(base64TestInput + "\nhexToBase64:\n" + output + "\n")
+} else {
+  print("Failed to convert")
+}
+
+let xorTestInput1 = "1c0111001f010100061a024b53535009181c"
+let xorTestInput2 = "686974207468652062756c6c277320657965"
+result = String(bytes: crypto.xorHexStrings([UInt8](xorTestInput1.utf8), hex2: [UInt8](xorTestInput2.utf8))!, encoding: NSUTF8StringEncoding)
+if let output2 = result {
+  print(xorTestInput1 + ", " + xorTestInput2 + "\nxorHexStrings:\n" + output2 + "\n")
+} else {
+  print("Failed to xor")
+}
+
+let singleByteXorInput = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+result = String(bytes: crypto.decipherSingleByteXorHex([[UInt8]](arrayLiteral: [UInt8](singleByteXorInput.utf8)))!, encoding: NSUTF8StringEncoding)
+if let output3 = result {
+  print(singleByteXorInput + "\ndecipherSingleByteXorHex:\n" + output3 + "\n")
+} else {
+  print("Failed to decipher")
+}
+
+do {
+  let singleByteXor2Input = try NSString(contentsOfFile: "/Volumes/HD/programming_stuff/exercises/crypto/4.txt", encoding: NSUTF8StringEncoding)
+  let lines = singleByteXor2Input.componentsSeparatedByString("\r\n")
+  result = String(bytes: crypto.decipherSingleByteXorHex(lines.map { [UInt8]($0.utf8) })!, encoding: NSUTF8StringEncoding)
+  if let output4 = result {
+    print("decipherSingleByteXorHex (with 4.txt):\n" + output4 + "\n")
+  } else {
+    print("Failed to decipher")
+  }
+} catch {
+  print("Failed to read input file")
+}
